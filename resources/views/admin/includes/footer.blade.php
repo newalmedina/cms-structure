d2e2dc7645fc073761dd5e64224f8e9eeeb88275
Newal medina <ing.newal.medina@gmail.com>
2021-06-26 04:56:43 +0800

    cambiando footer

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -5,5 +5,5 @@
         <strong>{{ trans("general/admin_lang.version") }}</strong> {{ config('general.app_version') }}
     </div>
     <!-- Default to the left -->
-    <strong>{{ trans("general/admin_lang.copy") }} &copy; {{ date("Y") }} <a href="http://www.aduxia.com" target="_blank">Aduxia</a>.</strong> {{ trans("general/admin_lang.rights") }}
+    <strong>{{ trans("general/admin_lang.copy") }} &copy; {{ date("Y") }} <a href="#" target="_blank">Newal Medina</a>.</strong> {{ trans("general/admin_lang.rights") }}
 </footer>
